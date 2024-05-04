da7e76ac4817747fded1edbd02b662097431f84f
Erfanmah7 <efn8mah@yahoo.com>
2024-05-04 21:06:53 +0330

    Add css inline

--- src/Coursecard.jsx
+++ src/Coursecard.jsx
@@ -1,7 +1,28 @@
-function Coursecard({ id, title }) {
+import { useState } from "react";
+
+// eslint-disable-next-line react/prop-types
+function Coursecard({ title }) {
+  const [mystyle, setStyle] = useState(false);
+
+  const colorHandeler = () => {
+    setStyle((mystyle) => !mystyle);
+  };
+
+  const cssStyles = {
+    color: "azure",
+    backgroundColor: "cadetblue",
+    padding: "5px 10px",
+    border: "none",
+    borderRadius: "10px",
+    margin: "20px",
+  };
+
   return (
     <>
-      <li>{title}</li>
+      <li style={{ color: mystyle ? "red" : "blue" }}>{title}</li>
+      <button style={cssStyles} onClick={colorHandeler}>
+        ChangeColor
+      </button>
     </>
   );
 }
