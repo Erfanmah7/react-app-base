832f4528f2044cd4f417f71b7f674837dda7e5ca
Erfanmah7 <efn8mah@yahoo.com>
2024-05-04 21:48:16 +0330

    Getting props by styled-components

--- src/Courselist.jsx
+++ src/Courselist.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import Coursecard from "./Coursecard.jsx";
 import styled from "styled-components";
 
+const Title = styled.h2`
+  color: ${(props) => (props.data ? "red" : "green")};
+  font-size: 2rem;
+`;
+
 function Courselist() {
+  const [css, setCss] = useState(true);
+
   const courses = [
     { id: "1", title: "html" },
     { id: "2", title: "css" },
@@ -10,14 +18,12 @@ function Courselist() {
     { id: "5", title: "ract" },
   ];
 
-  const Title = styled.h2`
-    color: red;
-    font-size: 2rem;
-  `;
-
   return (
     <>
-      <Title>salam man erfan hastam :)</Title>
+      <Title data={css}>salam man erfan hastam :)</Title>
+      <br />
+      <button onClick={() => setCss((css) => !css)}>Change</button>
+      <br />
       <br />
       <ul>
         {courses.map((course) => (
